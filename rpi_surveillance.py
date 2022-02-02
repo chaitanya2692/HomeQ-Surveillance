45654701eaff3e82174965f3c141871a4642c447
Chaitanya <chaitanya2692@gmail.com>
2022-02-02 23:48:58 +0100

    Add code to run on rpi
    A simple surveillance system to send a message on whataspp
    when it spots an apartment with the specified criteria
    according to the url.

---
+++ rpi_surveillance.py
@@ -0,0 +1,67 @@
+import csv
+import itertools
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+import webbrowser as web
+import pyautogui as pg
+import os
+
+chrome_options = Options()
+# chrome_options.add_argument("--headless")
+
+def runner():
+    url = "https://www.homeq.se/search?roomMin=2&areaMin=50&rentMax=10000&is_everyone=true&is_student=true&selectedShapes=urban_area.4368%3B5e076ebbc599fa845a722a9a378eb5e2742628a8317c7703145432ae2f78477c%3BG%C3%B6teborg"
+    browser = webdriver.Chrome(executable_path='chromedriver', options=chrome_options)
+    browser.get(url)
+    # time.sleep(5)
+    html = browser.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    browser.quit()
+    list_homes = []
+    list_new_homes = []
+    filename = 'all_homes.csv'
+
+    f = open(filename, 'a+', encoding="utf-8")
+    f.close()
+
+    with open(filename, 'r', encoding="utf-8") as csv_file:
+        rf = csv.reader(csv_file)
+        list_homes = list(itertools.chain(*rf))
+    csv_file.close()
+
+    with open(filename, 'a+', newline='', encoding="utf-8") as csv_file:
+        wf = csv.writer(csv_file)
+        for tag in soup.find_all('a', href=True):
+            if "https://www.homeq.se/lagenhet/" in tag['href']:
+                curr_url = str(tag['href'])
+                if curr_url not in list_homes:
+                    print(curr_url)
+                    list_new_homes.append([curr_url])
+                    wf.writerow([curr_url])
+
+    csv_file.close()
+    time.sleep(1)
+    # TODO - send message cleanly
+    if len(list_new_homes) != 0:
+        print("Sending new apartments")
+        numbers = ["+46735799272"]
+        for phone in numbers:
+            for home in list_new_homes: 
+                message = "https://web.whatsapp.com/send?phone=" + phone + "&text=" + home[0]
+                web.open(message)            
+                time.sleep(80)
+                pg.press('esc')
+                time.sleep(10)
+                pg.press('enter')
+                time.sleep(10)
+                pg.hotkey('ctrl', 'w')
+    return
+
+
+if __name__ == "__main__":
+    runner()
+    t = time.localtime()
+    print("Slept at", time.strftime("%H:%M:%S", t))
+        
